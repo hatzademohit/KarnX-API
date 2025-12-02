bb1519fa651daddf63e242a3418e6a6eedcc0a45
AzureAD\MukeshKshirsagar <mkshirsagar@valethi.com>
2025-12-02 15:22:31 +0530

    ddd

--- app/Http/Controllers/Api/Dashboard/KXManagerController.php
+++ app/Http/Controllers/Api/Dashboard/KXManagerController.php
@@ -139,7 +139,7 @@ class KXManagerController extends Controller
             if ($item->flightDetails) {
                 $dep = $item->flightDetails[0]->departure_location;
                 $arr = $item->flightDetails[0]->arrival_location;
-                $dep = AirportCities;
+                $dep = AirportCities::find($dep)->code;
                 $arr = AirportCities::find($arr)->code;
                 $route = $dep . ' → ' . $arr;
             }
